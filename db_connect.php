57c1c5e17d1053c1bdbcb71de125ebec90b576d6
Roland Steinebrunner <43014086+RSteinebrunner@users.noreply.github.com>
2024-01-30 23:34:39 -0800

    update db

--- db_connect.php
+++ db_connect.php
@@ -4,17 +4,21 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 // modify these settings according to the account on your database server.
-$host = "roundhouse.proxy.rlwy.net";
-$port = "54211";
-$username = "root";
-$user_pass = "cAGD51F13bG3c6AA4a32bb4eBHdFh1CD";
-$database_in_use = "railway";
+$host = getenv("RAILWAY_DATABASE_HOST");
+$port = getenv("RAILWAY_DATABASE_PORT");
+$username = getenv("RAILWAY_DATABASE_USERNAME");
+$user_pass = getenv("RAILWAY_DATABASE_PASSWORD");
+$database_in_use = getenv("RAILWAY_DATABASE_NAME");
 
 
-$mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
+
+$mysqli = new mysqli($host, $username, $user_pass, $database_in_use, $port);
 if ($mysqli->connect_error) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+        die("Connection failed: " . $mysqli->connect_error);
+    // echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
+$timeout_seconds = 5;
+$mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, $timeout_seconds);
 echo $mysqli->host_info . "<br>";
 
 ?>
